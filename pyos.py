cb0fbd83b4a22d58c44a105f651eec10ae25ac4c
Matheus Henrique de Oliveria <mthws.henrique@hotmail.com>
2022-05-19 09:01:21 -0300

    Update pyos.py

--- pyos.py
+++ pyos.py
@@ -4,9 +4,8 @@ import pycfg
 from pyarch import load_binary_into_memory
 from pyarch import cpu_t
 
-
 class os_t:
-	def __init__(self, cpu, memory, terminal):
+	def __init__ (self, cpu, memory, terminal):
 		self.cpu = cpu
 		self.memory = memory
 		self.terminal = terminal
@@ -14,81 +13,37 @@ class os_t:
 		self.terminal.enable_curses()
 
 		self.console_str = ""
-		self.command = ""
-		self.terminal.console_print("Bem vindo ao console, digite os comandos.\n\n")
+		self.terminal.console_print("this is the console, type the commands here\n")
 
 	def printk(self, msg):
 		self.terminal.kernel_print("kernel: " + msg + "\n")
 
-	def panic(self, msg):
+	def panic (self, msg):
 		self.terminal.end()
 		self.terminal.dprint("kernel panic: " + msg)
 		self.cpu.cpu_alive = False
 		#cpu.cpu_alive = False
 
-	def interrupt_keyboard(self):
+	def interrupt_keyboard (self):
 		key = self.terminal.get_key_buffer()
 
-		if ((key >= ord('a')) and (key <= ord('z'))) or ((key >= ord('A')) and (key <= ord('Z'))) or ((key >= ord('0')) and (key <= ord('9'))) or (key == ord(' ')) or (key == ord('-')) or (key == ord('_')) or (key == ord('.')) or (key == ord(',')):
-			# JUNTA A STRING QUE O USUARIO DIGITAR AO CONSOLE
-			self.console_str += chr(key)
-			# IMPRIMI O QUE ESTA NA VARIAVEL STRING
-			self.terminal.console_print("\r" + self.console_str)
-			# "\r" RETORNO REAL DA STRING (EVITA ERROS COM CARACTER ESPECIAL)
-
+		if ((key >= ord('a')) and (key <= ord('z'))) or ((key >= ord('A')) and (key <= ord('Z'))) or ((key >= ord('0')) and (key <= ord('9'))) or (key == ord(' ')) or (key == ord('-')) or (key == ord('_')) or (key == ord('.')):
+			strchar = chr(key)
+			self.console_str+=strchar
+			
 		elif key == curses.KEY_BACKSPACE:
-			# REMOVE O ULTIMO CARACTER DA STRING
-			self.console_str = self.console_str[:-1]
-			# IMPRIME O QUE ESTA NA STRING
-			self.terminal.console_print("\r" + self.console_str)
+			self.console_str[:-1]
+			self.terminal.console_print("\r"+self.console_str)
 			return
-
 		elif (key == curses.KEY_ENTER) or (key == ord('\n')):
-			# VERIFICA SE ESTA VAZIO OU SE CONTEM CARACTERES ESPECIAIS
-			if (self.console_str == "" or self.console_str.isspace()):
-				self.terminal.console_print("Digite caracteres validos \n")
-				self.console_str = ""
-
-			else:
-				self.terminal.console_print("\n")
-				
-				if(self.console_str == "sair"):
-					exit()  # DIGITE "sair" PARA SAIR DO SISTEMA
-
-				if(self.console_str[0:3] == "iniciar"):
-					# COMANDO SEM EFEITO NO SISTEMA
-					self.command = self.console_str[4:]
-					self.syscall()
-
-				self.console_str = ""
-
 			return
 
-	def handle_interrupt(self, interrupt):
-
-		# VERIFICA SE HA INTERRUPCAO DO TECLADO E SE HOUVER SEGUE PARA O METODO ABAIXO
-		if interrupt == pycfg.INTERRUPT_KEYBOARD:
-			self.interrupt_keyboard()
+	def handle_interrupt (self, interrupt):
+		if(interrupt==pycfg.INTERRUPT_KEYBOARD):
+			self.interrupt_keyboard
+			self.terminal.console_print("\r"+self.console_str)
 		return
 
-	def syscall(self):
-		self.terminal.console_print("Loading Process{}".format(self.command))
-
-		if(self.command == "idle"): 
-			self.terminal.app_print("idle nao foi implementado")
-		elif(self.command == "perfect-squares"):
-			self.terminal.app_print("perfect-squares nao foi implementado")
-		elif(self.command == "print"):
-			self.terminal.app_print("print nao foi implementado")
-		elif(self.command == "print2"):
-			self.terminal.app_print("print2 nao foi implementado")
-		elif(self.command == "test-gpf"):
-			self.terminal.app_print("test-gpf nao foi implementado")
-		elif(self.command == "teste"):
-			self.terminal.app_print("teste nao foi implementado")
-		else:
-			self.terminal.console_print("O comando {} nao existe".format(self.command))
-
-		self.command = ""
-
+	def syscall (self):
+		#self.terminal.app_print(msg)
 		return
