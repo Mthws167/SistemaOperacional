4a5e485abd8313aa45072598a375e4f310db1b93
Matheus Henrique de Oliveria <mthws.henrique@hotmail.com>
2022-10-19 20:56:50 -0300

    Update pysim.py

--- pysim.py
+++ pysim.py
@@ -48,3 +48,95 @@ else:
 	cpu.set_pc(1)
 
 main()
+
+
+
+import curses
+import sys
+import pycfg
+from pyarch import *
+from pyos import *
+
+def main ():
+	while cpu.cpu_alive:
+		cpu.run_cycle()
+		memory.run_cycle()
+		timer.run_cycle()
+		terminal.run_cycle()
+
+	terminal.end()
+
+	if cpu.sim_mode_os == 0:
+		terminal.dprint(cpu.regs)
+		terminal.dprint(memory.data)
+	
+	terminal.dprint("pysim halted")
+
+if len(sys.argv) == 1:
+	sim_mode_os = 1
+else:
+	sim_mode_os = 0
+
+terminal = terminal_t()
+
+if sim_mode_os == 1:
+	memsize = pycfg.MEMORY_SIZE_WITH_OS
+else:
+	memsize = pycfg.MEMORY_SIZE_WITHOUT_OS
+
+memory = memory_t(terminal, memsize)
+cpu = cpu_t(terminal, memory)
+timer = timer_t(cpu)
+
+terminal.set_cpu(cpu)
+
+cpu.sim_mode_os = sim_mode_os
+
+if cpu.sim_mode_os == 1:
+	os = os_t(cpu, memory, terminal)
+	cpu.set_os(os)
+	terminal.set_os(os)
+else:
+	load_binary_into_memory(sys.argv[1], memory, 0)
+	cpu.set_pc(1)
+
+main()
+
+
+
+import curses
+
+NO_KEY_PRESSED = -1
+
+stdscr = curses.initscr()
+stdscr.nodelay(True)
+
+# Ativa teclado
+stdscr.keypad(True)
+
+# Refresh after attributes
+stdscr.refresh()
+
+# No echo to screen
+curses.noecho()
+
+# Remove cursor
+curses.curs_set(0)
+
+# Cor
+if curses.has_colors():
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+
+win1 = curses.newwin(0,0,0,0)
+win1.box()
+win1.addstr(1,1,"Para sair pressione a tecla 'q'!")
+
+
+key_pressed = NO_KEY_PRESSED
+while key_pressed != ord('q'):
+    key_pressed = stdscr.getch()
+    stdscr.refresh()
+    win1.refresh()
+
+curses.endwin()
